48b0fc7084cf16aa5e8fc13e6e9591e9d78960d9
v.adhithyan <aavispeaks@gmail.com>
2016-05-13 13:18:52 +0530

    setup files modified

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = "awesome-bing",
+	package = ["awesome-bing"],
+	version = 0.1,
+	description = "A python utility to view today's bing wallpaper, its information and set as wallpaper",
+	author = "Adhithyan V",
+	email = "aavispeaks@gmail.com",
+	url = "https://github.com/v-adhithyan/awesome-bing",
+	download_url = "https://github.com",
+	keywords = [ 'bing', 'wallpaper', 'download'],
+	classifiers = []
+)
