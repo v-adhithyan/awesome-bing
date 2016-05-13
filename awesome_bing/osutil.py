8b3780975aa5eee8c0cdbf283220fe253f6d860e
v.adhithyan <aavispeaks@gmail.com>
2016-05-13 22:08:54 +0530

    pic directory manipulation util added

--- awesome_bing/osutil.py
+++ awesome_bing/osutil.py
@@ -1,4 +1,30 @@
 import platform
+import os
 
 def get_os_name():
 	return platform.system()
+
+def check_and_make_directory(pic_dir):
+	if not os.path.exists(pic_dir):
+		print "Creating new directory ...\n"
+		print "The path to new directory is: " + pic_dir
+		os.makedirs(pic_dir)
+	else:
+		print "Directory exists"
+
+def get_pic_directory():
+	os_name = get_os_name().lower()
+	pic_dir = ""
+	bing_pics = os.path.sep + "Pictures" + os.path.sep + "bing"
+
+	if(os_name == "darwin" or os_name == "linux"):
+		if(os.environ.has_key("HOME")):
+			pic_dir = os.environ.get("HOME")
+			pic_dir = pic_dir  + bing_pics
+	elif(os_name == "windows"):
+		print "Windows"
+	else:
+		print "Unsupported os."
+
+	check_and_make_directory(pic_dir)
+	return pic_dir
