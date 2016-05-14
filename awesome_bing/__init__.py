9d1d9ca49fe18dcb7442a7b2f7ae43b60be11bda
v.adhithyan <aavispeaks@gmail.com>
2016-05-14 13:21:59 +0530

    unwanted code removed

--- awesome_bing/__init__.py
+++ awesome_bing/__init__.py
@@ -3,19 +3,16 @@ import osutil
 
 def help():
     return """** awesome-bing **\n
-    Use awesome-bing with any of the following options:                                        \n        
-    * help - to know about the list of options available.                                        \n
-    * info - to get the story associated with the image of the day.                              \n
-    * set - to get image of the day from bing and set as wallpaper.                              \n
+    Use awesome-bing with any of the following options:\n        
+    * help - to know about the list of options available.\n
+    * info - to get the story associated with the image of the day.\n
+    * set - to get image of the day from bing and set as wallpaper.\n
     ****For example type "awesome-bing info" without quotes the get today's image story from bing. \n"""
 
 def info():
     bing.info()
 
-def getset():
+def set():
 	fname = bing.download_image()
 	print "Setting the downloaded image as wallpaper .."
 	osutil.set_wallpaper(fname)
-
-def pic_dir():
-    return osutil.get_pic_directory()
